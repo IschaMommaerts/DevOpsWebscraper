1959f97f519ab2c20e5840161c2e940a78d26275
IschaMommaerts <r0835633@student.thomasmore.be>
2021-12-19 18:54:49 +0100

    directory change test

--- SeleniumTest.cs
+++ SeleniumTest.cs
@@ -21,7 +21,7 @@ namespace WebScraper
         public void setup()
         {
             string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-            dv = new ChromeWebDriverRepository(path + @"\Drivers");
+            dv = new ChromeWebDriverRepository(path + @"/Drivers");
             vr = new VideoRepository();
             jr = new JobRepository();
 
